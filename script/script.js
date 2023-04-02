c7ca31e28b65cf44674659f72c2d2b83ac2bdf88
Sergey Terpugov <TerSV@yandex.ru>
2023-04-02 16:00:33 +0500

    Реализовано закрытие форм по крестику

--- script/script.js
+++ script/script.js
@@ -49,3 +49,39 @@ const buttonEdit = document.querySelector('.profile__edit-button');
 buttonEdit.addEventListener('click', () => {
     document.querySelector('.popup-edit').classList.add('popup-edit_opened');
 })
+
+/* Закрытие формы по крестику */
+/* const buttonClose = document.querySelector('.form__close');
+
+buttonClose.addEventListener('click', () => {
+    document.querySelector('.popup-edit').classList.remove('popup-edit_opened');
+}) */
+
+
+
+/*Открытие формы добавления картинок*/
+const buttonAdd = document.querySelector('.profile__add-button');
+
+
+buttonAdd.addEventListener('click', () => {
+    document.querySelector('.popup-add').classList.toggle('popup-add_opened');
+});
+
+
+/* Закрытие всех формы  через крестик*/
+
+const buttonsClose = document.querySelectorAll('.form__close');
+
+buttonsClose.forEach((buttonClose) => {
+    buttonClose.addEventListener('click', () => {
+        document.querySelector('.popup-edit').classList.remove('popup-edit_opened');
+        document.querySelector('.popup-add').classList.remove('popup-add_opened');
+    })
+})
+
+
+/* Добавление карточек */
+
+const namePicInput = document.querySelector('.form__item_el_name-pic');
+console.log(namePicInput.value);
+console.log(namePicInput.value);
